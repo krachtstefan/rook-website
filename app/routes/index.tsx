2aee8ee8c71bd59eb797749fd5a13099b6d0f102
Stefan Kracht <stefankracht@gmail.com>
2025-04-02 08:45:13 +0200

    more space

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute("/")({
 
 function Home() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-5">
+    <div className="flex h-dvh flex-col items-center justify-center p-10">
       <Logo className="size-auto max-h-[500px] max-w-[500px] fill-[#7900FF]" />
     </div>
   );
