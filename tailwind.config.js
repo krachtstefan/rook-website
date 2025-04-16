2ababbe77ce00e12741e1bdf270bd9c42598dd6c
Stefan Kracht <stefankracht@gmail.com>
2025-04-16 10:29:38 +0200

    source shadow config from css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -63,6 +63,14 @@ export default {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      boxShadow: {
+        DEFAULT: "var(--shadow)",
+        sm: "var(--shadow-sm)",
+        md: "var(--shadow-md)",
+        lg: "var(--shadow-lg)",
+        xl: "var(--shadow-xl)",
+        "2xl": "var(--shadow-2xl)",
+      },
       keyframes: {
         "accordion-down": {
           from: {
