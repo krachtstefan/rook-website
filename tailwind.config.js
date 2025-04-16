dbc3fcb314de1b7224d24b2a1cc77ea38c799632
Stefan Kracht <stefankracht@gmail.com>
2025-04-16 11:30:26 +0200

    use JetBrains mono

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ export default {
       },
     },
     extend: {
+      fontFamily: {
+        mono: ["JetBrains Mono", "monospace"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
