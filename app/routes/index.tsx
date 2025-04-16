021872b366d6f3625c5cef8aca9e58254b61f82c
Stefan Kracht <stefankracht@gmail.com>
2025-04-16 10:19:51 +0200

    add card components and create basic theme

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 import { Logo } from "./-components/logo";
 import { RookComputerMK2 } from "./-components/rook-computer-mk2";
 import { createFileRoute } from "@tanstack/react-router";
@@ -10,10 +11,12 @@ export const Route = createFileRoute("/")({
 function Home() {
   return (
     <div className="flex h-dvh flex-col items-center justify-center">
+      <Card className="p-10">
+        <Button>Click me</Button>
+      </Card>
       <Logo className="m-10 size-auto max-h-[500px] max-w-[500px] fill-rook-violet" />
       <div className="absolute inset-0 -z-10 text-[100px]">
         <RookComputerMK2 />
-        <Button>Click me</Button>
       </div>
     </div>
   );
