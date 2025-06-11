36cc0b0cbe7beab6e3eddfbd155a7981e51422f9
Stefan Kracht <stefankracht@gmail.com>
2025-06-11 15:25:13 +0200

    update images

--- app/routes/-components/gallery.tsx
+++ app/routes/-components/gallery.tsx
@@ -7,18 +7,59 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 
+import { ComponentProps } from "react";
+import image1 from "../../assets/images/01.jpg";
+import image2 from "../../assets/images/02.jpg";
+import image3 from "../../assets/images/03.webp";
+import image4 from "../../assets/images/04.png";
+import image5 from "../../assets/images/05.png";
+import image6 from "../../assets/images/06.png";
+import image7 from "../../assets/images/07.png";
+
+type ImageProps = ComponentProps<"img">;
+const images: ImageProps[] = [
+  {
+    src: image1,
+    alt: "Image 1",
+  },
+  {
+    src: image2,
+    alt: "Image 2",
+  },
+  {
+    src: image3,
+    alt: "Image 3",
+  },
+  {
+    src: image4,
+    alt: "Image 4",
+  },
+  {
+    src: image5,
+    alt: "Image 5",
+  },
+  {
+    src: image6,
+    alt: "Image 6",
+  },
+  {
+    src: image7,
+    alt: "Image 7",
+  },
+];
+
 export function Gallery() {
   return (
     <div className="flex flex-col items-center justify-center gap-8">
       <h1 className="text-5xl">Gallery</h1>
       <Carousel className="w-full max-w-xl">
         <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
+          {images.map((image, index) => (
             <CarouselItem key={index}>
               <div className="p-1">
                 <Card>
                   <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">{index + 1}</span>
+                    <img {...image} />
                   </CardContent>
                 </Card>
               </div>
