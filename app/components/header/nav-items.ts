95e1bd05fe0cc92c2f598d0f180d20cca0adfe7a
Stefan Kracht <stefankracht@gmail.com>
2025-06-18 08:05:54 +0200

    hide nav

--- app/components/header/nav-items.ts
+++ app/components/header/nav-items.ts
@@ -16,42 +16,42 @@ type MenuWithSubmenuLink = {
 type NavigationLink = MenuLink | MenuWithSubmenuLink;
 
 export const navigationLinks: Array<NavigationLink> = [
-  { type: "link", label: "Home", href: "#" },
-  {
-    label: "Features",
-    type: "submenu",
-    items: [
-      {
-        label: "Components",
-        href: "#",
-      },
-      {
-        label: "Documentation",
-        href: "#",
-      },
-      {
-        label: "Templates",
-        href: "#",
-      },
-    ],
-  },
-  {
-    label: "Pricing",
-    type: "submenu",
-    items: [
-      { label: "Product A", href: "#" },
-      { label: "Product B", href: "#" },
-      { label: "Product C", href: "#" },
-      { label: "Product D", href: "#" },
-    ],
-  },
-  {
-    label: "About",
-    type: "submenu",
-    items: [
-      { label: "Getting Started", href: "#" },
-      { label: "Tutorials", href: "#" },
-      { label: "About Us", href: "#" },
-    ],
-  },
+  // { type: "link", label: "Home", href: "#" },
+  // {
+  //   label: "Features",
+  //   type: "submenu",
+  //   items: [
+  //     {
+  //       label: "Components",
+  //       href: "#",
+  //     },
+  //     {
+  //       label: "Documentation",
+  //       href: "#",
+  //     },
+  //     {
+  //       label: "Templates",
+  //       href: "#",
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: "Pricing",
+  //   type: "submenu",
+  //   items: [
+  //     { label: "Product A", href: "#" },
+  //     { label: "Product B", href: "#" },
+  //     { label: "Product C", href: "#" },
+  //     { label: "Product D", href: "#" },
+  //   ],
+  // },
+  // {
+  //   label: "About",
+  //   type: "submenu",
+  //   items: [
+  //     { label: "Getting Started", href: "#" },
+  //     { label: "Tutorials", href: "#" },
+  //     { label: "About Us", href: "#" },
+  //   ],
+  // },
 ] as const;
