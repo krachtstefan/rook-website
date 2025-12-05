da1185f2c451be2d120a4a96381aecc5f505a6b7
Stefan Kracht <stefankracht@gmail.com>
2025-12-05 09:49:23 +0100

    move schema

--- app/api/client.ts
+++ app/api/client.ts
@@ -1,45 +1,9 @@
+import { PostSchema } from "./schemas";
 import { Zodios, makeApi } from "@zodios/core";
-
 import { env } from "../env";
 import { z } from "zod";
 
-const PostSchema = z.object({
-  id: z.number(),
-  date: z.string(),
-  date_gmt: z.string(),
-  guid: z.object({
-    rendered: z.string(),
-  }),
-  modified: z.string(),
-  modified_gmt: z.string(),
-  slug: z.string(),
-  status: z.string(),
-  type: z.string(),
-  link: z.string(),
-  title: z.object({
-    rendered: z.string(),
-  }),
-  content: z.object({
-    rendered: z.string(),
-    protected: z.boolean(),
-  }),
-  excerpt: z.object({
-    rendered: z.string(),
-    protected: z.boolean(),
-  }),
-  author: z.number(),
-  featured_media: z.number(),
-  comment_status: z.string(),
-  ping_status: z.string(),
-  sticky: z.boolean(),
-  template: z.string(),
-  format: z.string(),
-  meta: z.any(),
-  categories: z.array(z.number()),
-  tags: z.array(z.number()),
-});
-
-export type Post = z.infer<typeof PostSchema>;
+export * from "./schemas";
 
 const apiDefinition = makeApi([
   {
