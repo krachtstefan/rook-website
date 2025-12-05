6d5241e7ae7dc647cf5fe929f1c2b1ced5d9de13
Stefan Kracht <stefankracht@gmail.com>
2025-12-05 06:06:57 +0100

    add query client to router

--- app/router.tsx
+++ app/router.tsx
@@ -1,10 +1,16 @@
+import { QueryClient } from "@tanstack/react-query";
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
 export function createRouter() {
+  const queryClient = new QueryClient();
+
   const router = createTanStackRouter({
     routeTree,
     scrollRestoration: true,
+    context: {
+      queryClient,
+    },
   });
 
   return router;
