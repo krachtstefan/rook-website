82e859590cb8b24f79a00af5536657db01064260
Stefan Kracht <stefankracht@gmail.com>
2025-12-16 21:25:12 +0100

    update tagline

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -69,7 +69,7 @@ export default function Component() {
       <div className="grow">
         <a href="/" className="flex w-max items-center gap-2 text-primary">
           <Logo className="h-[42px]" />
-          <span className="whitespace-nowrap uppercase">R∞k Komputer</span>
+          <span className="whitespace-nowrap uppercase">Retro ∞ Konsole</span>
         </a>
       </div>
       <NavigationMenu viewport={false} className="max-md:hidden">
