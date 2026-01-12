0db6eab351475b7bcea292e22ed5bc910bf79cd9
Stefan Kracht <stefankracht@gmail.com>
2026-01-12 06:22:38 +0100

    bottom padding

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <Header />
-      <main className="mx-auto flex w-full max-w-[900px] flex-col items-center gap-10 px-8 pt-10">
+      <main className="mx-auto flex w-full max-w-[900px] flex-col items-center gap-10 px-8 py-10">
         {children}
       </main>
     </>
